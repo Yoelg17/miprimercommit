b68763180dffc2b3444c8329f4f9d484b9430253
yoel <n00484909@upn.pe>
2026-03-24 14:53:20 -0500

    agrege saludo

---
+++ miproyecto/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su nombre:");
+            int nombre;
+            nombre = int.Parse(Console.ReadLine());
+            Console.WriteLine($"tu nombre es: {nombre}");
+        }
+    }
+}
---
+++ miproyecto/ConsoleApp1/ejer1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ejer1
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("holaaa mundossss");
+        }
+    }
+}
